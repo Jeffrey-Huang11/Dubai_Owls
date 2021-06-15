92ac117996ff9b26b3807f8d36a157ca5739ba7f
KarlWithK <jocelinc60@outlook.com>
2021-06-15 14:05:37 -0400

    Did some game logic

--- cah/lib/game_logic/game.dart
+++ cah/lib/game_logic/game.dart
@@ -2,21 +2,21 @@ import 'package:cah/db/DbCAHC.dart';
 import 'package:cah/models/white_cards.dart';
 
 class Game {
+  int judge = 0; // index of judge
+  late List<String> users;
   late Map<String, int> _gameScoreboard;
   late Map<String, Future<List<WhiteCards>>> _playersHands;
   DatabaseCAHC database = DatabaseCAHC.instance;
 
-  Game() {
-    database.getAllCardPacksID();
-  }
-
   Future init(List<String> usernames) async {
+    database.getAllCardPacksID();
     _gameScoreboard = Map<String, int>.fromIterable(usernames,
         key: (user) => user, value: (_) => 0);
     _playersHands = Map<String, Future<List<WhiteCards>>>.fromIterable(
         usernames,
         key: (user) => user,
         value: (_) => database.getWhiteCards(10));
+      users = usernames;
   }
 
   void updateUserScore(String username) {
@@ -35,9 +35,20 @@ class Game {
     return "scoreboard: " + _gameScoreboard.toString() + "\nPlayers Hands" + _playersHands.toString();
   }
 
-  /* Future<WhiteCard> pick_random_card(String username,) */
+  void makeJudge() {
+    judge = (judge + 1) % users.length;
+  }
 
-  /* void pick_random_card(String username, int number_of_cards) { */
-  /* given their hand, pick  */
-  /* } */
+  // TODO: only assumes the black card only has one underline
+  Future<WhiteCards> pickRandomCardBot(String username) async {
+    (await _playersHands[username]!).add((await database.getWhiteCards(1))[0]);
+    return (await _playersHands[username]!).removeAt(0);
+  }
+  
+  // Assumes frontend gives a index of card picked and the username of player
+  // user[0] -> player's Name
+  Future<WhiteCards> pickCard(int cardChoice) async {
+    (await _playersHands[users[0]]!).add((await database.getWhiteCards(1))[0]);
+    return (await _playersHands[users[0]]!).removeAt(cardChoice);
+  }
 }
--- cah/lib/main.dart
+++ cah/lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:cah/screens/signIn.dart';
-void main() => runApp(SignUp());
+import 'package:cah/game_logic/game.dart';
+
+void main() async {
+  runApp(SignUp());
+  Game test = Game();
+  List<String> users = ["Arib", "Karl", "Joe", "Anya", "Jeff"];
+  test.init(users);
+  print(await test.getUserCards("Arib"));
+  print(await test.pickRandomCardBot("Arib"));
+  print(await test.getUserCards("Arib"));
+}
 
 // shows the routes of the website and which function to invoke
 class SignUp extends StatelessWidget {
