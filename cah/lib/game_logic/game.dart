336ad03b1f3026a3e45077018f64e0d475378f15
KarlWithK <jocelinc60@outlook.com>
2021-06-20 17:34:50 -0400

    Renamed to better format

--- cah/lib/game_logic/game.dart
+++ cah/lib/game_logic/game.dart
@@ -8,9 +8,9 @@ class Game {
   late Map<String, Future<List<WhiteCards>>> _playersHands;
   late Map<int, WhiteCards>
       selectedCards; //Game sees cards in order of usernames
-  late bool game_done = false; //If game_done, cut to end screen
-  late String game_winner = ""; //Winner of game if game_winner != ""
-  late int score_limit = 0;
+  late bool gameDone = false; //If game_done, cut to end screen
+  late String gameWinner = ""; //Winner of game if game_winner != ""
+  late int scoreLimit = 0;
   DatabaseCAHC database = DatabaseCAHC.instance;
 
   Future init(List<String> usernames) async {
@@ -33,8 +33,8 @@ class Game {
     _gameScoreboard.update(username, (value) => ++value);
   }
 
-  void setScoreLimit(int score_limit) {
-    score_limit = score_limit;
+  void setScoreLimit(int scoreLimit) {
+    scoreLimit = scoreLimit;
   }
 
   Future<List<WhiteCards>> getUserCards(String username) async {
@@ -59,7 +59,7 @@ class Game {
   String checkScore() {
     for (int i = 0; i < users.length; i++) {
       String user = users[i];
-      if (_gameScoreboard[user] == score_limit) {
+      if (_gameScoreboard[user] == scoreLimit) {
         return user;
       }
     }
@@ -69,12 +69,12 @@ class Game {
   // Assumes frontend gives a index of card picked and the username of player
   // Assumes 1 player, many bots
   Future<WhiteCards> pickCard(String username, int cardChoice) async {
-    WhiteCards player_chose_white =
+    WhiteCards playerChoseWhite =
         await pickCardHelper(username, cardChoice: cardChoice);
     for (int i = 1; i < users.length; i++) {
       await pickCardHelper(users[i]);
     }
-    return player_chose_white;
+    return playerChoseWhite;
   }
 
   // only assumes the black card only has one underline
@@ -83,18 +83,18 @@ class Game {
   Future<WhiteCards> pickCardHelper(String username,
       {int cardChoice: 0}) async {
     (await _playersHands[username]!).add((await database.getWhiteCards(1))[0]);
-    WhiteCards whites_only =
+    WhiteCards whitesOnly =
         (await _playersHands[username]!).removeAt(cardChoice);
-    selectedCards[users.indexOf(username)] = whites_only;
-    return whites_only;
+    selectedCards[users.indexOf(username)] = whitesOnly;
+    return whitesOnly;
   }
 
   WhiteCards pickAnswer(int cardChoice) {
     updateUserScore(users[cardChoice]);
     String user = checkScore();
     if (user != "") {
-      game_done = true;
-      game_winner = user;
+      gameDone = true;
+      gameWinner = user;
     }
     nextJudge();
     return selectedCards[cardChoice]!;
